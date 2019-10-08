7650dda161be51b20b855107a71882efe3abf213
jcshim <jcshim@andong.ac.kr>
2019-10-09 00:21:31 +0900

    초기 커밋.

---
+++ MainPage.xaml.cpp
@@ -0,0 +1,33 @@
+﻿//
+// MainPage.xaml.cpp
+// MainPage 클래스의 구현입니다.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace U_Hello;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x412에 나와 있습니다.
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+
+void U_Hello::MainPage::Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	greetingOutput->Text = "Hello, " + nameInput->Text + "!";
+}
---
+++ MainPage.xaml.h
@@ -0,0 +1,23 @@
+﻿//
+// MainPage.xaml.h
+// MainPage 클래스의 선언입니다.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace U_Hello
+{
+	/// <summary>
+	/// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	private:
+		void Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+}
